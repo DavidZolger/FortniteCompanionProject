bfbe5935fb1ea3e2c443dbdc7a945f62ed1dbbd9
Miha Podplatnik <miha.podplatnik@gmail.com>
2018-01-21 16:43:13 +0100

    -Dodano shranjevanje v bazo -Dodan SugarORM za delanje z bazo

--- app/src/main/java/um/feri/rvir/fortnitecompanionproject/MainActivity.java
+++ app/src/main/java/um/feri/rvir/fortnitecompanionproject/MainActivity.java
@@ -12,6 +12,7 @@ import com.google.gson.Gson;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Logger;
 
 import okhttp3.OkHttpClient;
@@ -36,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
         /*
         * Zapa malo mi nekaj
          */
+
+        List<IgralecSimple> igralecSimpleList = IgralecSimple.listAll(IgralecSimple.class);
+        if(igralecSimpleList == null || igralecSimpleList.size() == 0){
+            new IgralecSimple().save();
+        }
     }
 
     public void poizveduj(View view) {
@@ -64,6 +70,9 @@ public class MainActivity extends AppCompatActivity {
 
                 IgralecFull igralec = new Gson().fromJson(jsonData, IgralecFull.class);
                 IgralecSimple igralecSimple = IgralecSimplifier.vPreprostegaIgralca(igralec);
+                igralecSimple.save();
+
+                //IgralecSimple igralecPridobljen = (IgralecSimple) IgralecSimple.find(IgralecSimple.class, "accountId = ?", "87dccf37-41bc-42f5-918d-4ff871106a68");
 
                 System.out.println(igralecSimple);
 
